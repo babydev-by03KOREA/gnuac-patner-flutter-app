913cd3c210152f3794bd52e2eb815f9529df8eca
parkhyeongju_macmini <hyeongju1122@gmail.com>
2025-11-11 02:08:09 +0900

    :sparkles: 기본적인 Flutter 프로젝트 생성완료 with Supabase

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+  await Supabase.initialize(
+    url: dotenv.get("PROJECT_URL"),
+    anonKey: dotenv.get("PROJECT_API_KEY"),
+  );
+  // runApp();
+}
